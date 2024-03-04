158bfc73b98b59d7e05389d5033b31201c832cfc
Ganesh Dhore <prathamesherande@bdo.in>
2024-03-04 22:47:18 +0530

    REST DATA

---
+++ 27_SPRING_DATA_REST_CONCEPT_JAN_12/src/main/java/com/configuration/BookConfiguration.java
@@ -0,0 +1,27 @@
+package com.configuration;
+
+import java.awt.print.Book;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class BookConfiguration implements RepositoryRestConfigurer {
+
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+	    HttpMethod[] unsupportedMethods = { HttpMethod.PUT, HttpMethod.DELETE };
+
+	    config.getExposureConfiguration()
+	            .forDomainType(Book.class)
+	            .withItemExposure((metadata, http) -> http.disable(unsupportedMethods))
+	            .withCollectionExposure((metadata, http) -> http.disable(unsupportedMethods));
+	}
+
+
+}
